2f7156704e379adf158c3ac0ae342c38173e3f43
haydnkeung <haydnkeung@gmail.com>
2019-04-21 17:09:56 -0400

    Added has function

--- Trie.cc
+++ Trie.cc
@@ -9,6 +9,7 @@ class Trie{
 public:
 	Trie();
 	bool isEmpty()const;
+	bool contains(const string str)const;
 	void print()const;
 	void insert(const string str);
 private:
@@ -16,12 +17,9 @@ private:
 	Node* root;
 	Node* insert(Node* root, const char c);
 	void insert(Node* root, const string str);
-	void print(Node *,string str)const;
+	void print(Node * root,string str)const;
+	Node* contains(Node * root, const char)const;
 };
-bool Trie::isEmpty()const{
-	return nullptr == root;
-}
-
 
 struct Trie::Node{
 	char c;
@@ -33,8 +31,6 @@ Trie::Trie(): root{new Node}{
 	root->c= 0;
 	root->isLeaf = true;
 }
-
-
 
 Trie::Node* Trie::insert(Node* root, const char c){
 	Node * temp = new Node;
@@ -62,6 +58,10 @@ void Trie::insert(const string str){
 		ptr = insert(ptr,i);
 	}
 	
+}
+
+bool Trie::isEmpty()const{
+	return nullptr == root;
 }
 
 void Trie::print(Node * root,string str)const{
@@ -93,9 +93,30 @@ void Trie::print()const{
 }
 
 
+
+
+bool Trie::contains(const string str)const{
+	Node * ptr = this->root;
+	for(auto i:str){
+		ptr = contains(ptr,i);
+		if(nullptr==ptr){
+			return false;
+		}
+	}
+	return true;
+}
+
+Trie::Node* Trie::contains(Node * root,const char c)const{
+	int pos = c - 'a';
+	if(nullptr==root->arr[pos]){
+		return nullptr;
+	}else{
+		return root->arr[pos];
+	}
+}
+
 int main(int argc, char* argv[]){
 	Trie trie;
-
 
 	ifstream stream("twl.txt");
 	string input;
@@ -108,5 +129,6 @@ int main(int argc, char* argv[]){
 	// trie.insert("dog");
 	cout<<trie.isEmpty()<<endl;
 	trie.print();
+	cout<<trie.contains("zedhhhhhhhhhhs")<<endl;
 
 }
